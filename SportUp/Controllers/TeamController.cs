e8084d72d120c85f4f9b03e6cf09d020f3161f92
John Singh <wethegreenpeople@gmail.com>
2020-02-11 20:20:21 -0800

    Add ability to create team

---
+++ SportUp/Controllers/TeamController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportUp.Data;
+using SportUp.Data.Models;
+using SportUp.Models.ViewModels;
+
+namespace SportUp.Controllers
+{
+    [Authorize]
+    public class TeamController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<SportUpUser> _userManager;
+
+        public TeamController(ApplicationDbContext context, UserManager<SportUpUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTeam(TeamIndexViewModel viewModel)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            viewModel.Team.UserTeams = new List<UserTeam>
+            {
+                new UserTeam()
+                {
+                    SportUpUser = currentUser,
+                }
+            };
+            await _context.Teams.AddAsync(viewModel.Team);
+            await _context.SaveChangesAsync();
+
+            return View("Index");
+        }
+    }
+}
---
+++ SportUp/Models/ViewModels/TeamIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SportUp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportUp.Models.ViewModels
+{
+    public class TeamIndexViewModel
+    {
+        public Team Team { get; set; }
+    }
+}
